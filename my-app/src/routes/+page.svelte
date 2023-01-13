0f7cf7317aa583a275d7c24f57735b16cf276822
Paul Thomas <paulthomas2107@gmail.com>
2023-01-13 15:40:41 +0000

    first commit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+</svelte:head>
+
+<div style="margin: 0 auto; padding: 20px; width: 700px;">
+    <h2 style="text-align: center;">ToDo List</h2>
+    <p>Enter your ToDo here</p>
+    <div style="display: flex;">
+        <input type="text">
+        <button style="width: 200px;">Add</button>
+    </div>
+</div>
